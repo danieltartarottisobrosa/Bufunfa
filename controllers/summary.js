8f63904ee88515cfd7599648cf596669493b9f9f
danieltartarottisobrosa <danieltsobrosa@gmail.com>
2014-02-25 10:31:11 -0300

    Criada visualisão resumida (implementação inicial)

--- controllers/summary.js
+++ controllers/summary.js
@@ -1,16 +1,46 @@
 'use strict';
 
 var Entry = require( '../models/entry' ),
-	Group = require( '../models/group' );
+	Group = require( '../models/group' ),
+	_ = require( 'lodash');
 
 module.exports = function ( app ) {
 
 	app.get( '/summary', function ( req, res ) {
+		var model = {
+			groups: []
+		};
+		
+		function requestEnd() {
+			res.render( 'summary', model );
+		}
+		
+		function groupEntriesReceived( group, i, len ) {
+			return function( err, entries ) {
+				var onlyValues = _.pluck( entries, 'value' );
+				group.entries = entries;
+				
+				group.total = _.reduce( onlyValues, function( sum, value ) {
+					return sum + value;
+				});
+				
+				// Finaliza o request
+				if ( i === len - 1 ) {
+					requestEnd();
+				}
+			};
+		}
 		
 		Group.find( {}, function( err, groups ) {
+			var i = 0,
+				len = groups.length;
 			
-			
-			res.render( 'summary', {} );
+			for ( ; i < len; i++ ) {
+				var group = groups[ i ];
+				model.groups.push( group );
+				
+				Entry.find( { group: group.id }, groupEntriesReceived( group, i, len ) );
+			}
 		});
 	});
 
